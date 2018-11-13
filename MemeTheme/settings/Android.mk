24cf26e544860d2db23fc90735649f17b316ba6e
eldianasor <hernan@gmail.com>
2018-11-13 15:53:14 +0000

    Moar updoots

--- MemeTheme/settings/Android.mk
+++ MemeTheme/settings/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := ShishuAmalgamationThemeSettingsOverlay
+LOCAL_PACKAGE_NAME := MemeThemeSettingsOverlay
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
